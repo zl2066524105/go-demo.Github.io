4ae88e440c969184219494576b62d14f3df8afd7
江南烟雨 <2066524105@qq.com>
2020-11-14 10:15:22 +0800

    Initial commit

--- src/main.go
+++ src/main.go
@@ -2,11 +2,19 @@ package main
 
 import "fmt"
 
-func main()  {
-		a :=97
-		x :=19968
-		result :=string(a)
-		fmt.Println(result)
-		result=string(x)
-		fmt.Println(result)
+func main() {
+	i := 1
+	sum := 0
+	for i <= 40 {
+		if i%3 == 0 {
+			sum += i
+			fmt.Printf("%d", i)
+			if i < 39 {
+				fmt.Printf("+")
+			} else {
+				fmt.Printf("=%d", sum)
+			}
+		}
+		i++
+	}
 }
