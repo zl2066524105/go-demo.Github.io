a92a5955d486b8c55608749064ffbbddc88937f5
江南烟雨 <2066524105@qq.com>
2020-11-14 10:15:22 +0800

    Initial commit

--- src/main.go
+++ src/main.go
@@ -2,11 +2,32 @@ package main
 
 import "fmt"
 
-func main()  {
-		a :=97
-		x :=19968
-		result :=string(a)
-		fmt.Println(result)
-		result=string(x)
-		fmt.Println(result)
+func main() {
+	var grade string
+	score := 78.5
+	switch {
+	case score >= 90:
+		grade = "A"
+	case score >= 80:
+		grade = "B"
+	case score >= 70:
+		grade = "C"
+	case score >= 60:
+		grade = "D"
+	default:
+		grade = "E"
+	}
+	fmt.Printf("%s\n", grade)
+	switch grade {
+	case "A":
+		fmt.Printf("优秀\n")
+	case "B":
+		fmt.Printf("良好\n")
+	case "C":
+		fmt.Printf("中等\n")
+	case "D":
+		fmt.Printf("及格\n")
+	default:
+		fmt.Printf("差\n")
+	}
 }
