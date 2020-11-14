c1d23b80a8e740f3816dbc8bdd563bddcae2495e
江南烟雨 <2066524105@qq.com>
2020-11-14 16:26:40 +0800

    Initial commit

--- src/main.go
+++ src/main.go
@@ -3,18 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	i := 1
-	sum := 0
-	for i <= 40 {
-		if i%3 == 0 {
-			sum += i
-			fmt.Printf("%d", i)
-			if i < 39 {
-				fmt.Printf("+")
-			} else {
-				fmt.Printf("=%d", sum)
-			}
+	for i := 1; i <= 9; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Printf("%d*%d=%2d ", j, i, j*i)
 		}
-		i++
+		fmt.Printf("\n")
 	}
 }
