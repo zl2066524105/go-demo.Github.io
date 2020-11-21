00e4b3fafcb22c6e676dec8275b26ad14317f025
江南烟雨 <2066524105@qq.com>
2020-11-21 10:57:12 +0800

    Initial commit

--- src/main.go
+++ src/main.go
@@ -2,9 +2,34 @@ package main
 
 import "fmt"
 
+type processFunc func(int) bool
+
 func main() {
-	username := ""
-	age := 0
-	_, _ = fmt.Scanln(&username, &age)
-	fmt.Println("帐号信息为：", username, age)
+	slice := []int{1, 2, 3, 4, 5, 7}
+	fmt.Println("slice = ", slice)
+	odd := filter(slice, isOdd)
+	fmt.Println("奇数元素：", odd)
+	even := filter(slice, isEven)
+	fmt.Println("偶数元素:", even)
+}
+func isEven(integer int) bool {
+	if integer%2 == 0 {
+		return true
+	}
+	return false
+}
+func isOdd(integer int) bool {
+	if integer%2 == 0 {
+		return false
+	}
+	return true
+}
+func filter(slice []int, f processFunc) []int {
+	var result []int
+	for _, value := range slice {
+		if f(value) {
+			result = append(result, value)
+		}
+	}
+	return result
 }
